76281b36504dc9041666e7de316e6aeb70d76462
Marcio Campos <marciocampos@tokenlab.com.br>
2018-05-14 15:24:28 -0300

    React router setup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import 'normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Boilerplate</p>, document.getElementById("app"));
+const ExpenseDashboardPage = () => (
+  <div>Dashboard works!</div>
+);
+
+const AddExpensePage = () => (
+  <div>Add expense works!</div>
+);
+
+const EditExpensePage = () => (
+  <div>Edit expense works!</div>
+);
+
+const HelpPage = () => (
+  <div>Help works!</div>
+);
+
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+      <Route path="/create" component={AddExpensePage}/>
+      <Route path="/edit" component={EditExpensePage}/>
+      <Route path="/help" component={HelpPage}/>
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
